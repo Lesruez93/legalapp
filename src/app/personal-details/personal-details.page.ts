d6e5f186e8fdc233188e87724973879449c2a110
Aaron <aaronkatema@gmail.com>
2021-09-16 14:35:25 +0200

    change

--- src/app/personal-details/personal-details.page.ts
+++ src/app/personal-details/personal-details.page.ts
@@ -17,54 +17,7 @@ export class PersonalDetailsPage implements OnInit {
   country_phone_group: FormGroup;
   countries: Array<CountryPhone>;
   genders: Array<string>;
-  
-  validations = {
-    'username': [
-      { type: 'required', message: 'Username is required.' },
-      { type: 'minlength', message: 'Username must be at least 5 characters long.' },
-      { type: 'maxlength', message: 'Username cannot be more than 25 characters long.' },
-      { type: 'pattern', message: 'Your username must contain only numbers and letters.' },
-      { type: 'usernameNotAvailable', message: 'Your username is already taken.' }
-    ],
-    'name': [
-      { type: 'required', message: 'Name is required.' }
-    ],
-    'idtype': [
-      { type: 'required', message: 'ID Type is required.' }
-    ],
-    'lastname': [
-      { type: 'required', message: 'Last name is required.' }
-    ],
-    'email': [
-      { type: 'required', message: 'Email is required.' },
-      { type: 'pattern', message: 'Enter a valid email.' }
-    ],
-    'phone': [
-      { type: 'required', message: 'Phone is required.' },
-      { type: 'invalidCountryPhone', message: 'Phone is incorrect for the selected country.' }
-    ],
-    'password': [
-      { type: 'required', message: 'Password is required.' },
-      { type: 'minlength', message: 'Password must be at least 5 characters long.' },
-      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase and one number.' }
-    ],
-    'confirm_password': [
-      { type: 'required', message: 'Password confirmation is required.' }
-    ],
-    'matching_passwords': [
-      { type: 'areNotEqual', message: 'Password mismatch' }
-    ],
-    'guests': [
-      { type: 'rangeError', message: 'Number must be between: ' }
-    ],
-    'bedrooms': [
-      { type: 'rangeError', message: 'Number must be between: ' }
-    ],
-    'terms': [
-      { type: 'pattern', message: 'You must accept terms and conditions.' }
-    ]
-  };
-  // countries: any;
+
 
   constructor() { }
 
@@ -83,43 +36,8 @@ export class PersonalDetailsPage implements OnInit {
     this.genders = [
       'Female',
       'Male',
-      'Other'
     ];
 
 
-
-    const country = new FormControl(this.countries[0], Validators.required);
-    
-    const phone = new FormControl('', Validators.compose([
-      Validators.required,
-      PhoneValidator.invalidCountryPhone(country)
-    ]));
-    this.country_phone_group = new FormGroup({
-      country: country,
-      phone: phone
-    });
-
-    this.validationsForm = new FormGroup({
-      //   'username': new FormControl('', Validators.compose([
-      //     UsernameValidator.usernameNotAvailable,
-      //     Validators.maxLength(25),
-      //     Validators.minLength(5),
-      //     Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
-      //     Validators.required
-      //   ])),
-      //   'name': new FormControl('', Validators.required),
-      //   'lastname': new FormControl('', Validators.required),
-      //   'email': new FormControl('', Validators.compose([
-      //     Validators.required,
-      //     Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
-      //   ])),
-      //   'gender': new FormControl(this.genders[0], Validators.required),
-      //   'country_phone': this.country_phone_group,
-      //   'matching_passwords': this.matching_passwords_group,
-      //   'guests': new FormControl(6, counterRangeValidator(1, 12)),
-      //   'bedrooms': new FormControl(3, counterRangeValidator(1, 5)),
-      //   'terms': new FormControl(true, Validators.pattern('true'))
-      // });
-    })
   }
 }
